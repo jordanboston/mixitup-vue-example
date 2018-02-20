0b0ca69ca725417e1d8e1f94c7e179de03db326c
Jordan Boston <jordandboston@gmail.com>
2018-02-20 10:22:04 -0800

    Change gitignore to allow the dist dir
    Update assets path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/mixitup-vue-example',
+    publicPath: '/mixitup-vue-example/dist/',
     filename: 'build.js'
   },
   module: {
